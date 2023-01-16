0d3f28521b3db071d2a0a4864adefad43cbf79d4
Fellipe Furtado <fellipe.furtado@hotmail.com>
2023-01-16 16:41:35 -0300

    database update

--- src/Api.Data/Context/MyContext.cs
+++ src/Api.Data/Context/MyContext.cs
@@ -11,7 +11,7 @@ namespace Api.Data.Context
     public class MyContext : DbContext
     {
         public DbSet<UserEntity> Users { get; set; }
-
+ 
         public MyContext(DbContextOptions<MyContext> options) : base(options)
         {
         }
