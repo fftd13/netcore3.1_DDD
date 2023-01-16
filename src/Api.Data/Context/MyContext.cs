48d8ae4c8617ed71a730e1646814b4aee211aefd
Fellipe Furtado <fellipe.furtado@hotmail.com>
2023-01-16 14:54:38 -0300

    Data: referencia a Domain/ criacao de pastas repository,mapping e context/ contrutor e override na classe MyContext

---
+++ src/Api.Data/Context/MyContext.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Context
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
